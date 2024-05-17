10d5a3cea3dcf07cd92e00999f89c62c0449abd0
Jasur <125241046+kamilov2@users.noreply.github.com>
2024-05-17 19:42:52 +0500

    Update signals.py

--- main/signals.py
+++ main/signals.py
@@ -1,22 +1,16 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from .models import News, Profile
-# from django.core.mail import send_mail
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import News, Profile
+from .email_utils import send_email  
 
-# @receiver(post_save, sender=News)
-# def notify_profiles_on_news_creation(sender, instance, created, **kwargs):
-#     if created:
-#         profiles = Profile.objects.all()
-#         subject = f'News: {instance.title}'
-#         message = f'News in site visit news: "{instance.title}".\n\n{instance.content} https://careconnect.uz/posts_detail/{instance.id}'
-#         recipient_list = [profile.email for profile in profiles]
-        
-#         print(recipient_list)
-        
-#         send_mail(
-#             subject,
-#             message,
-#             'ivpplatform@yandex.com',  
-#             recipient_list,
-#             fail_silently=False,
-#         )
+@receiver(post_save, sender=News)
+def notify_profiles_on_news_creation(sender, instance, created, **kwargs):
+    if created:
+        profiles = Profile.objects.all()
+        subject = f'News: {instance.title}'
+        message = f'News in site visit news: "{instance.title}".\n\n{instance.content} https://careconnect.uz/posts_detail/{instance.id}'
+        recipient_list = [profile.email for profile in profiles]
+        print(recipient_list)
+
+        send_email(subject, message, recipient_list)
+        # helo
