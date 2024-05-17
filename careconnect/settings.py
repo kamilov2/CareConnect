3e39ac30cf5392369179a086caa0be7b72f70f08
Jasur <125241046+kamilov2@users.noreply.github.com>
2024-05-17 13:31:41 +0300

    Update settings.py

--- careconnect/settings.py
+++ careconnect/settings.py
@@ -119,12 +119,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.com'
-EMAIL_PORT = '465'
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465  # 465 is commonly used for SSL; use 587 for TLS
+EMAIL_USE_TLS = False  # For port 465, typically SSL is used, not TLS
+EMAIL_USE_SSL = True  # Use SSL for port 465
 EMAIL_HOST_USER = 'ivpplatform@yandex.com'
 EMAIL_HOST_PASSWORD = 'efrlcevitfesgwit'
+
+# Additional settings if required
+DEFAULT_FROM_EMAIL = 'ivpplatform@yandex.com'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
