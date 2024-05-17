c64de5afab0a2fae3609d377c2cfcecd91c65db6
Jasur <125241046+kamilov2@users.noreply.github.com>
2024-05-17 13:22:09 +0300

    Update signals.py

--- main/signals.py
+++ main/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import News, Profile
-from .email_utils import send_email  
+from django.core.mail import send_mail
 
 @receiver(post_save, sender=News)
 def notify_profiles_on_news_creation(sender, instance, created, **kwargs):
