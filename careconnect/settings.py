882a07072f80b4f05b6fbd51e4faf0c125a27dde
Jasur <125241046+kamilov2@users.noreply.github.com>
2024-05-17 14:14:50 +0300

    Update settings.py

--- careconnect/settings.py
+++ careconnect/settings.py
@@ -125,7 +125,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465  # Correct port for SSL
 EMAIL_USE_TLS = False  # Set to False for SSL on port 465
-EMAIL_USE_SSL = True  # Enable SSL for secure email
+EMAIL_USE_SSL = False  # Enable SSL for secure email
 EMAIL_HOST_USER = 'ivpplatform@yandex.com'
 EMAIL_HOST_PASSWORD = 'efrlcevitfesgwit'
 
